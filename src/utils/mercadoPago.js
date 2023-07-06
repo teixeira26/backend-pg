7629437cd720545ee81aab90c0b0a1aac302d760
Mathzilla <teixeira.matheus26@gmail.com>
2023-07-06 11:55:53 -0300

    comment mp code

--- src/utils/mercadoPago.js
+++ src/utils/mercadoPago.js
@@ -1,9 +1,9 @@
 const mercadopago = require('mercadopago')
 require('dotenv').config();
 
-mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN,
-});
+// mercadopago.configure({
+//     access_token: process.env.ACCESS_TOKEN,
+// });
 
 module.exports = {
     mercadopago
